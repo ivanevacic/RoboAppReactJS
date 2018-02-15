b9d457467e92f85a7ac918f2b27e1f48d5c568a1
ivanevacic <ivanevacicweb@gmail.com>
2018-02-15 17:18:38 +0100

    Created and imported 'Card' component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+                <div>
+                  <Card />
+                  <Card />
+                  <Card />
+                </div>
+, document.getElementById('root'));
 registerServiceWorker();

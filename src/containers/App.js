07d5a117cca4b5d2b2c68b50f91f6916260a9e73
ivanevacic <ivanevacicweb@gmail.com>
2018-02-16 16:41:43 +0100

    Added proper folder structure,simplified parts of code

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 
 class App extends Component {
   constructor() {
@@ -24,10 +24,11 @@ class App extends Component {
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(robots => {
-    return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    const { robots, searchField } = this.state;
+    const filteredRobots = robots.filter(robots => {
+    return robots.name.toLowerCase().includes(searchField.toLowerCase());
     })
-    if(this.state.robots.length === 0) {
+    if(!robots.length) {
       return <h1>Loading</h1>
     } else {
       return (

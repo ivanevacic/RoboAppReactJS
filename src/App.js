1b5348d6bb489822581fc4f8913c95ef689d10ab
ivanevacic <ivanevacicweb@gmail.com>
2018-02-16 16:22:58 +0100

    Removed static robots.js file,switched to simple API to fetch robots

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ''
     }
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users}));   
+  }
+
 
   onSearchChange = (event) => {
     this.setState({ searchField: event.target.value })
@@ -20,13 +26,17 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robots => {
     return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
     })
-    return (
-      <div className='tc'>
-        <h1 className='f2'>RoboApp</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if(this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f2'>RoboApp</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }   
   }
 }
 
